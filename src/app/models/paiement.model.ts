dce791fdf19ae9740ac594e387db1db4fb3aa7ae
Salem Kamoun <kammoun.salem@gmail.com>
2025-10-31 08:45:55 +0100

    rm src

--- src/app/models/paiement.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Paiement {
-  id?: string;
-  marinId: string;
-  montant: number;
-  datePaiement: Date;
-  sortiesIds: string[]; // Liste des sorties concernées
-  description?: string;
-  createdAt?: Date;
-}
